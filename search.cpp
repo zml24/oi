93f9357bb4ad884412180fe652ce1f47ab21aefd
zml24 <sufezml@sina.com>
2020-09-01 20:17:11 +0800

    20.9.1

---
+++ search.cpp
@@ -0,0 +1,27 @@
+int bsearch_1(int l, int r) {
+    while (l < r) {
+        int mid = l + r >> 1;
+        if (check(mid)) r = mid;
+        else l = mid + 1;
+    }
+    return l;
+}
+
+int bsearch_2(int l, int r) {
+    while (l < r) {
+        int mid = l + r + 1 >> 1;
+        if (check(mid)) l = mid;
+        else r = mid - 1;
+    }
+    return l;
+}
+
+double bsearch(double l, double r) {
+    const double eps = 1e-6;
+    while (r - l > eps) {
+        double mid = (l + r) / 2;
+        if (check(mid)) r = mid;
+        else l = mid;
+    }
+    return l;
+}
