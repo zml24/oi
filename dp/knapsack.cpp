214d3a79e3fc8c586226af052536825ef26ea9cf
zml24 <sufezml@sina.com>
2021-02-03 16:56:07 +0800

    modify bounded knapsack

--- dp/knapsack.cpp
+++ dp/knapsack.cpp
@@ -12,10 +12,10 @@ for (int i = 1; i <= n; i++) {
     for (int j = 0; j < v[i]; j++) {
         int hh = 0, tt = -1;
         for (int k = j; k <= m; k++) {
-            if (hh <= tt && q[hh] < k - s * v) hh++;
-            while (hh <= tt && g[q[tt]] - (q[tt] - j) / v * w <= g[k] - (k - j) / v * w) tt--;
+            if (hh <= tt && q[hh] < k - s * v[i]) hh++;
+            while (hh <= tt && g[q[tt]] - (q[tt] - j) / v[i] * w[i] <= g[k] - (k - j) / v[i] * w[i]) tt--;
             q[++tt] = k;
-            f[k] = g[q[hh]] + (k - q[hh]) / v * w;
+            f[k] = g[q[hh]] + (k - q[hh]) / v[i] * w[i];
         }
     }
 }
